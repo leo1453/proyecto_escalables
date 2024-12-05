833baa80a379fd79506ac2f04bba7b86a91385ad
leo <ldkl12@hotmail.com>
2024-12-04 22:18:54 -0600

    Reorganización del proyecto: añadido Angular y Node.js, respaldo del proyecto antiguo

--- src/app/models/message.model.ts
+++
@@ -1,8 +0,0 @@
-export interface Message {
-    sender: string; // ID del remitente
-    receiver: string; // ID del receptor
-    content: string; // Contenido del mensaje
-    timestamp: Date; // Marca de tiempo del mensaje
-    senderName?: string; // Nombre del remitente (opcional)
-  }
-  
--- src/app/models/user.model.ts
+++
@@ -1,8 +0,0 @@
-// src/app/models/user.model.ts
-export interface User {
-    _id: string;
-    name: string;
-    email: string;
-    // Agrega más propiedades según sea necesario
-  }
-  
